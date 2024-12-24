1062af138927fb6beb0f49a55bd036ea71cdfac6
Eric Windmill <eric@ericwindmill.com>
2024-12-24 15:44:00 -0500

    initial

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,10 +1,48 @@
 import "tsx/esm";
+import * as path from 'node:path';
+import * as sass from 'sass';
 
 export default function (eleventyConfig: any) {
+    eleventyConfig.addTemplateFormats("scss");
+    eleventyConfig.addWatchTarget('./src/_sass');
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+        compileOptions: {
+          cache: false,
+        },
+        compile: async function (inputContent: any, inputPath: string) {
+            const parsedPath = path.parse(inputPath);
+            if (parsedPath.name.startsWith("_")) {
+                return;
+            }
+
+            let result = sass.compileString(inputContent, {
+                loadPaths: [
+                    parsedPath.dir || '.',
+                ],
+            });
+
+            this.addDependencies(inputPath, result.loadedUrls);
+            // This is the render function, `data` is the full data cascade
+            return async () => {
+                return result.css;
+            };
+        },
+    });
+
+    eleventyConfig.setServerOptions({
+        port: 4000,
+        watch: ['src/_sass'],
+    });
+
     return {
+        htmlTemplateEngine: 'liquid',
         dir: {
-            input: "src",
-            output: "_site",
+            input: 'src/content',
+            output: '_site',
+            layouts: '../_layouts',
+            includes: '../_includes',
+            data: '../_data',
         },
     };
 }
---
+++ src/content/index.md
@@ -0,0 +1,27 @@
+---
+title: "&#128679; Eric Windmill &#128679;"
+layout: default
+---
+
+## Links
+
+* [Flutter][] and [Dart][]
+* [Github][]
+* [Flutter in Action][]
+
+## Work
+
+* Google, Developer Relations Engineer, [Dart][] and [Flutter][], Since 2021
+* Discovery Inc, Software Engineer, 2020-21
+* Flutter in Action, [Published 2019][]
+* Greenbits, Software Engineer (now [Greenbits by Dutchie][]) 2018-20
+* AppTree, Software Engineer, 2017-18
+
+
+[Flutter]: https://docs.flutter.dev 
+[Dart]: https://dart.dev
+[Flutter getting started documentation]: https://docs.flutter.dev/get-started/learn-flutter
+[GitHub]: https://github.com/ericwindmill
+[Flutter in Action]: https://www.oreilly.com/library/view/flutter-in-action/9781617296147/
+[Published 2019]:  https://www.oreilly.com/library/view/flutter-in-action/9781617296147/
+[Greenbits by Dutchie]: https://business.dutchie.com/greenbits
